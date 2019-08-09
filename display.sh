1af2ddbe17284eedc56113a7761ce02608142972
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2019-08-09 15:18:48 -0700

    a

--- display.sh
+++ display.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ "$#" -ne 1 ]; then
-  echo "example: $0 5727.2"
-  exit 1
+if [ "$#" -eq 1 ]; then
+  INPUT="$1"
+  TEMP=$(mktemp -d)
+  pushd $TEMP
+  aws-okta exec fullstack -- aws s3 cp --recursive "s3://optly-fs-travisci-artifacts/optimizely/full-stack-benchmark-suite/${INPUT%.*}/$INPUT/mprof/" .
+  open $TEMP/home/travis/mprof/*.png
+else
+  echo "example: $0 589.1"
 fi
-
-INPUT="$1"
-TEMP=$(mktemp -d)
-pushd $TEMP
-aws-okta exec fullstack -- aws s3 cp --recursive "s3://optly-fs-travisci-artifacts/optimizely/full-stack-benchmark-suite/${INPUT%.*}/$INPUT/sar/" .
-echo "data downloaded to $TEMP/home/travis/sarviewer/data"
-open $TEMP/home/travis/sarviewer/graphs/*.png
